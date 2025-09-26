b59699ac012765a9381a8aca0f0e0633814d5842
whitneykageni-a11y <whitneykageni@gmail.com>
2025-09-26 00:05:16 -0700

    Update units_y1s1.c

--- units_y1s1.c
+++ units_y1s1.c
@@ -1,6 +1,11 @@
-//Program to display units for Y1S1
+/*
+Name:Whitnay Kageni
+Reg No:CT101/G/26543/25
+Program:Display of units for Y1S1
+*/
 # include <stdio.h>
 int main(){
     printf("\nThese are my units:\nIntroduction to computer architecture\nFundamentals of ICT\nIntroduction to programming and problem solving\nMathematics for science\nDiscrete mathematics\n");
     return 0;
 }
+
