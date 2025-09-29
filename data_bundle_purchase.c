d6fc4cf85c5d432efbb702628569b4615e5b418b
whitneykageni-a11y <whitneykageni@gmail.com>
2025-09-29 23:41:54 +0300

    Add files via upload

---
+++ data_bundle_purchase.c
@@ -0,0 +1,33 @@
+/*
+Name:Whitney Kageni
+Reg No:CT101/G/26543/25
+Program:Data_bundle_purchase
+*/
+# include <stdio.h>
+int main (){
+    int number;
+
+    printf("\nAvailable data bundles:\n\n1. 100MB-50kes\n\n2. 500MB-200kes\n\n3. 1GB  -350kes\n\n4. 2GB  -600kes\n");
+
+    printf("Choose your data bundle(1-4)\n");
+    scanf("%d",&number);
+
+    switch (number){
+        case 1:
+               printf("You have chosen 100MB amount to be paid 50kes");
+               break;
+        case 2:
+               printf("You have chosen 500MB amount to be paid 200kes");
+               break;
+        case 3:
+               printf("You have chosen 1GB amount to be paid 350kes");
+               break;
+        case 4:
+               printf("You have chosen 2GB amount to be paid 600kes");
+               break;
+        default:
+               printf("invalid choice");
+               break;
+    }
+    return 0;
+}
