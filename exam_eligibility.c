e4cbecd1bdebeba3f36ec4b0460a777ac59523c1
whitneykageni-a11y <whitneykageni@gmail.com>
2025-09-30 09:37:07 +0300

    Add files via upload

---
+++ exam_eligibility.c
@@ -0,0 +1,24 @@
+/*
+Name:Whitney Kageni
+Reg No:CT101/G/26543/25
+Description:program to determine the eligibility of a student for exams
+*/
+# include <stdio.h>
+int main (){
+    int average_marks;
+    float attendance_in_percentage;
+
+    printf("Enter your average marks:\n");
+    scanf("%d",&average_marks);
+
+    printf("Enter your class attendance(in percentage):\n");
+    scanf("%f",&attendance_in_percentage);
+
+    if(average_marks>=40&&attendance_in_percentage>=75){
+        printf("\nYou are eligible.\n");
+    }
+    else{
+        printf("\nNot eligible.\n");
+    }
+    return 0;
+}
---
+++ water_billcalculation.c
@@ -0,0 +1,27 @@
+/*
+Name:Whitney Kageni
+Reg No:CT101/G/26543/25
+Description:program to computer water bills
+*/
+# include <stdio.h>
+int main (){
+    int water_units;
+    double total_water_bill;
+
+    printf("Enter your water units:\n");
+    scanf("%d",&water_units);
+
+    if(water_units<=30){
+        total_water_bill=water_units*20;
+    }
+    else if(water_units>=31&& water_units<=60){
+        total_water_bill=water_units*25;
+    }
+    else{
+        total_water_bill=water_units*30;
+    }
+
+    printf("\nWater units:%d\n",water_units);
+    printf("\nTotal water bill %.2lfkes\n",total_water_bill);
+    return 0;
+}
